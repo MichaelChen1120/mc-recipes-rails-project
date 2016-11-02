0ef14d1b318e46977519634220da70859b386ae8
Michael Chen <michael.chen.1120@gmail.com>
2016-11-02 22:18:22 +0000

    generated resource for users and recipe ingredients

--- recipes/config/routes.rb
+++ recipes/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :recipe_ingredients
   resources :ingredients
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- recipes/db/migrate/20161102215346_create_recipes.rb
+++ recipes/db/migrate/20161102215346_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[5.0]
   def change
     create_table :recipes do |t|
-      
+      t.string :name
       t.timestamps
     end
   end
