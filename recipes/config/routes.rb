eeb924a47d183cc908247063f42259897be086f1
Michael Chen <michael.chen.1120@gmail.com>
2016-11-07 23:34:51 +0000

    added routes

--- recipes/config/routes.rb
+++ recipes/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :recipe_ingredients
   resources :ingredients
   resources :recipes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
