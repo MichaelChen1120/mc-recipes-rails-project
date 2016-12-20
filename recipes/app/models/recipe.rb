2cb33f5e25ae88ba45354bb0130939076b49dc43
Michael Chen <michael.chen.1120@gmail.com>
2016-12-20 19:48:05 +0000

    created dynamic input fields for recipe form

--- recipes/app/models/recipe.rb
+++ recipes/app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   has_many :reviews
-  accepts_nested_attributes_for :ingredients, :allow_destroy => true
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
 
   def ingredients_attributes=(ingredient_attributes)
     ingredient_attributes.values.each do |ingredient_attribute|
