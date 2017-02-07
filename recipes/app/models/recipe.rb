c63d91f8ed2f3e4e3ec31ade601e381ae7fc36b3
Michael Chen <michael.chen.1120@gmail.com>
2017-02-07 01:18:59 +0000

    added average_rating method to recipe model

--- recipes/app/models/recipe.rb
+++ recipes/app/models/recipe.rb
@@ -14,4 +14,42 @@ class Recipe < ApplicationRecord
     end
   end
 
+  def rated?
+    if self.reviews != nil
+      true
+    else
+      false
+    end
+  end
+
+  def self.highest_rated
+    recipes = []
+    Recipe.all.each do |recipe|
+      if recipe.rated?
+        recipes << recipe
+      end
+    end
+    recipes.sort.reverse
+  end
+
+  def sorted
+    self.reviews
+  end
+
+  def sort_by_rating
+    sorted_reviews = []
+    reviews = []
+    self.reviews.each do |review|
+      reviews << review.rating
+    end
+  end
+
+  def average_rating
+    ratings = []
+    self.reviews.each do |review|
+      ratings << review.rating
+    end
+    ratings.sum / ratings.size_to_i
+  end
+
 end
